45a03867aa0e4336532a19e1087e122574ce10b8
usuario <usuario@10.22.143.6>
2015-10-15 13:01:19 +0100

    1 version

---
+++ src/kata3/KAta3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata3;
+
+/**
+ *
+ * @author usuario
+ */
+public class KAta3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("adios");
+        
+        
+    }
+}
